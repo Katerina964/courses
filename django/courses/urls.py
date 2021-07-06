65257b4f9d6165d58db68b31266ab22eaa5e8c1f
Katerina964 <katrin.balakina@gmail.com>
2021-07-06 11:23:22 +0300

    url changed

--- django/courses/urls.py
+++ django/courses/urls.py
@@ -4,10 +4,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'courses'
 urlpatterns = [
-    path('api/report/<int:pk>/', views.StudentReport.as_view(), name='report'),
-    path('api/course/', views.CourseList.as_view(), name='list'),
-    path('api/participant/<int:pk>/', views.ParticipantDelete.as_view(), name='delete'),
-    path('api/create/', views.ParticipantCreate.as_view(), name='create')
+    path('report/<int:pk>/', views.StudentReport.as_view(), name='report'),
+    path('course/', views.CourseList.as_view(), name='list'),
+    path('participant/<int:pk>/', views.ParticipantDelete.as_view(), name='delete'),
+    path('create/', views.ParticipantCreate.as_view(), name='create')
     ]
 
 urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'csv'])
