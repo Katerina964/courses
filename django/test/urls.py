1a5bdf49d5a5f16c3a954eb6ff6b0a643fd90bd0
Katerina964 <katrin.balakina@gmail.com>
2021-07-06 12:51:41 +0300

    next

--- django/test/urls.py
+++ django/test/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'api/', include('courses.urls')),
+    path(r'api', include('courses.urls')),
     path('admin/', admin.site.urls),
 ]
