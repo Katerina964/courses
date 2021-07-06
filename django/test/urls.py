04664ea71596dce44942ddae6fc16bceea269480
Katerina964 <katrin.balakina@gmail.com>
2021-07-06 13:51:32 +0300

    changed url

--- django/test/urls.py
+++ django/test/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'', include('courses.urls')),
+    path('api/', include('courses.urls')),
     path('admin/', admin.site.urls),
 ]
